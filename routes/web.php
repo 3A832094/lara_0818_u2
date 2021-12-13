4057f957dc4102194cf137391215b6da56231b7a
3A832094 <3a832094@gm.student.ncut.edu.tw>
2021-12-13 10:17:05 +0800

    重做 練習1-8：改用Home1Controller回傳

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\Home1Controller
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,3 +23,4 @@ Route::get('hello/{name?}',[HelloController::class,'index'])->name('hello.index'
 //--------------------------------------------------------------------------------------------
 Route::get('home1', function (){return view('home1');});
 Route::get('home1', function (){return view('home1.index');});
+Route::get('home1',[\App\Http\Controllers\Home1Controller::class,'index'])->name('home1.index');
